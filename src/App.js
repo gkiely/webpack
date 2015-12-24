232933c8e75e6cd0552f8461c3f0020a20aea8dc
Grant <grant.kiely@gmail.com>
2015-12-25 00:00:30 +1100

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Counter from './Counter';
 
+//import 'stylesheets/modules/container'
 
 var colors = {
   primary: "#777",
-  secondary:  "orange"
+  secondary:  "orange",
+  // secondary: "red"
 };
 
 
